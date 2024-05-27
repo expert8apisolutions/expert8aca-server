cc330df03329f568256497ed3fcf1cc7e2749b87
Teerapong <44111604+teera04164@users.noreply.github.com>
2024-05-27 22:28:54 +0700

    Update file.route.ts

--- routes/file.route.ts
+++ routes/file.route.ts
@@ -6,42 +6,60 @@ const fileRouter = express.Router();
 
 fileRouter.get(
     "/file/get-folders-files/:id?",
+    isAutheticated, 
+    authorizeRoles("admin"),
     getFolderAndFile,
 );
 
 fileRouter.post(
     "/file/get-folder/:id",
+    isAutheticated, 
+    authorizeRoles("admin"),
     getSingleFolder,
 );
 
 fileRouter.post(
     "/file/create-folder",
+    isAutheticated, 
+    authorizeRoles("admin"),
     createFolder,
 );
 
 fileRouter.post(
     "/file/create-file",
+    isAutheticated, 
+    authorizeRoles("admin"),
     addFile,
 );
 
 fileRouter.post(
     "/file/edit-file/:id",
+    isAutheticated, 
+    authorizeRoles("admin"),
     editFile,
 );
 fileRouter.post(
     "/file/edit-folder/:id",
+    isAutheticated, 
+    authorizeRoles("admin"),
     editFolder,
 );
 fileRouter.delete(
     "/file/delete-folder/:id",
+    isAutheticated, 
+    authorizeRoles("admin"),
     deleteFolder,
 );
 fileRouter.delete(
     "/file/delete-file/:id",
+    isAutheticated, 
+    authorizeRoles("admin"),
     deleteFile,
 );
 fileRouter.post(
     "/file/update-playback",
+    isAutheticated, 
+    authorizeRoles("admin"),
     updatePlayBackId,
 );
 export default fileRouter;
